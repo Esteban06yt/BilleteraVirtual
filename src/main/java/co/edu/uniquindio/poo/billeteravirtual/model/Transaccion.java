e749cad8a7d4285252a850195cac03085a490770
Esteban06yt <133072260+Esteban06yt@users.noreply.github.com>
2025-05-15 22:44:34 -0500

    Avance codigo: parte del model

--- src/main/java/co/edu/uniquindio/poo/billeteravirtual/model/Transaccion.java
+++ src/main/java/co/edu/uniquindio/poo/billeteravirtual/model/Transaccion.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 
 public class Transaccion {
     private final String idTransaccion;
-    private LocalDateTime fechaHora;
+    private final LocalDateTime fechaHora;
     private final Double monto;
     private final String descripcion;
     private final TipoTransaccion tipo;
@@ -14,27 +14,25 @@ public class Transaccion {
     private final Usuario emisor;
     private final Usuario destinatario;
 
-    public Transaccion(String idTransaccion, LocalDateTime fechaHora, Double monto, String descripcion, TipoTransaccion tipo, Categoria categoria, Usuario emisor, Usuario destinatario) {
-        this.idTransaccion = idTransaccion;
-        this.fechaHora = fechaHora;
-        this.monto = monto;
-        this.descripcion = descripcion;
-        this.tipo = tipo;
-        this.categoria = categoria;
-        this.emisor = emisor;
-        this.destinatario = destinatario;
+    // Constructor privado (utilizado solo por el Builder)
+    private Transaccion(Builder builder) {
+        this.idTransaccion = builder.idTransaccion;
+        this.fechaHora = builder.fechaHora;
+        this.monto = builder.monto;
+        this.descripcion = builder.descripcion;
+        this.tipo = builder.tipo;
+        this.categoria = builder.categoria;
+        this.emisor = builder.emisor;
+        this.destinatario = builder.destinatario;
     }
 
+    // Getters
     public String getIdTransaccion() {
         return idTransaccion;
     }
 
     public LocalDateTime getFechaHora() {
         return fechaHora;
-    }
-
-    public void setFechaHora(LocalDateTime fechaHora) {
-        this.fechaHora = fechaHora;
     }
 
     public Double getMonto() {
@@ -73,5 +71,62 @@ public class Transaccion {
                 ", emisor=" + emisor +
                 ", destinatario=" + destinatario +
                 '}';
+    }
+
+    // Builder
+    public static class Builder {
+        private String idTransaccion;
+        private LocalDateTime fechaHora;
+        private Double monto;
+        private String descripcion;
+        private TipoTransaccion tipo;
+        private Categoria categoria;
+        private Usuario emisor;
+        private Usuario destinatario;
+
+        public Builder withIdTransaccion(String idTransaccion) {
+            this.idTransaccion = idTransaccion;
+            return this;
+        }
+
+        public Builder withFechaHora(LocalDateTime fechaHora) {
+            this.fechaHora = fechaHora;
+            return this;
+        }
+
+        public Builder withMonto(Double monto) {
+            this.monto = monto;
+            return this;
+        }
+
+        public Builder withDescripcion(String descripcion) {
+            this.descripcion = descripcion;
+            return this;
+        }
+
+        public Builder withTipo(TipoTransaccion tipo) {
+            this.tipo = tipo;
+            return this;
+        }
+
+        public Builder withCategoria(Categoria categoria) {
+            this.categoria = categoria;
+            return this;
+        }
+
+        public Builder withEmisor(Usuario emisor) {
+            this.emisor = emisor;
+            return this;
+        }
+
+        public Builder withDestinatario(Usuario destinatario) {
+            this.destinatario = destinatario;
+            return this;
+        }
+
+        // Metodo para construir la transacción
+        public Transaccion build() {
+            return new Transaccion(this);
+        }
     }
 }
