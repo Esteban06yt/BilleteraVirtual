b891d37ba760c11a627e6eda81b59be6837bdc24
Camilo-Mendoza <camilodelcastillo321@gmail.com>
2025-05-27 17:27:04 -0500

    ViewController fxml contection

--- src/main/java/co/edu/uniquindio/poo/billeteravirtual/viewController/AdministradorViewController.java
+++ src/main/java/co/edu/uniquindio/poo/billeteravirtual/viewController/AdministradorViewController.java
@@ -1,29 +1,27 @@
 package co.edu.uniquindio.poo.billeteravirtual.viewController;
 
+import co.edu.uniquindio.poo.billeteravirtual.controller.AdministradorController;
+import co.edu.uniquindio.poo.billeteravirtual.model.Administrador;
+import co.edu.uniquindio.poo.billeteravirtual.enums.RolAdministrador;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
 public class AdministradorViewController {
 
-
         @FXML
-        private TableColumn<?, ?> tbc_telefono;
-
-        @FXML
-        private TableView<?> tb_usuario;
+        private TableView<Administrador> tb_administrador;
 
         @FXML
         private Button btn_volver;
-
-        @FXML
-        private TableColumn<?, ?> tbc_rol;
-
+        
         @FXML
         private Text txt_titulo;
 
@@ -31,34 +29,57 @@ public class AdministradorViewController {
         private Button btn_crudUsuarios;
 
         @FXML
-        private TableColumn<?, ?> tbc_nombre;
-
-        @FXML
         private ImageView img_logo;
 
         @FXML
         private Button btn_modificarPerfil;
+        
+        @FXML
+        private AnchorPane ap_administrador;
+        
+        @FXML
+        private Button btn_verGraficas;
+
+        @FXML
+        private TableView<Administrador> tb_usuario;
+
+        @FXML
+        private TableColumn<Administrador, String> tbc_nombre;
+
+        @FXML
+        private TableColumn<Administrador, String> tbc_cedula;
+
+        @FXML
+        private TableColumn<Administrador, String> tbc_telefono;
+
+        @FXML
+        private TableColumn<Administrador, String> tbc_correo;
+
+        @FXML
+        private TableColumn<Administrador, RolAdministrador> tbc_rol;
 
         @FXML
         private Button btn_actualizar;
 
-        @FXML
-        private AnchorPane ap_administrador;
+        private AdministradorController administradorController;
 
         @FXML
-        private TableColumn<?, ?> tbc_cedula;
+        public void initialize() {
+                administradorController = new AdministradorController();
 
-        @FXML
-        private TableColumn<?, ?> tbc_correo;
+                tbc_nombre.setCellValueFactory(new PropertyValueFactory<>("nombreCompleto"));
+                tbc_cedula.setCellValueFactory(new PropertyValueFactory<>("cedula"));
+                tbc_telefono.setCellValueFactory(new PropertyValueFactory<>("telefono"));
+                tbc_correo.setCellValueFactory(new PropertyValueFactory<>("correo"));
+                tbc_rol.setCellValueFactory(new PropertyValueFactory<>("rol"));
 
-        @FXML
-        private Button btn_verGraficas;
+                cargarAdministrador();
+        }
 
-//        @FXML
-//        void 231e7f(
-//        ActionEvent event) {
-//
-//        }
+        private void cargarAdministrador() {
+                ObservableList<Administrador> admin = administradorController.getAdministradorLogueado();
+                tb_usuario.setItems(admin);
+        }
 
         @FXML
         void onVolver(ActionEvent event) {
@@ -67,7 +88,8 @@ public class AdministradorViewController {
 
         @FXML
         void onActualizar(ActionEvent event) {
-
+                administradorController.actualizarAdministrador();
+                cargarAdministrador();
         }
 
         @FXML
@@ -85,5 +107,7 @@ public class AdministradorViewController {
 
         }
 
+//                @FXML
+//                void 231e7f(ActionEvent event) {
 
 }
