b66dc250a398a75ad42b34c14f311223e37db5a3
Camilo-Mendoza <camilodelcastillo321@gmail.com>
2025-05-27 15:08:36 -0500

    ViewController fxml contection

--- src/main/java/co/edu/uniquindio/poo/billeteravirtual/viewController/GraficasViewController.java
+++ src/main/java/co/edu/uniquindio/poo/billeteravirtual/viewController/GraficasViewController.java
@@ -6,6 +6,14 @@ import co.edu.uniquindio.poo.billeteravirtual.model.Usuario;
 import javafx.fxml.FXML;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.ComboBox;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.StackedBarChart;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 
 import java.util.Map;
 
@@ -31,4 +39,71 @@ public class GraficasViewController {
             gastosChart.getData().add(slice);
         });
     }
+
+        @FXML
+        private NumberAxis y_numeroGastos;
+
+        @FXML
+        private StackedBarChart<?, ?> stacked_gastosMasComunes;
+
+        @FXML
+        private LineChart<?, ?> line_saldoPromedio;
+
+        @FXML
+        private Button btn_volver;
+
+        @FXML
+        private Text txt_subtitulo;
+
+        @FXML
+        private NumberAxis y_numeroTransacciones;
+
+        @FXML
+        private Text txt_titulo;
+
+        @FXML
+        private AnchorPane ap_graficas;
+
+        @FXML
+        private CategoryAxis x_usarios;
+
+        @FXML
+        private NumberAxis y_saldo;
+
+        @FXML
+        private ImageView img_logo;
+
+        @FXML
+        private CategoryAxis x_tipoGasto;
+
+        @FXML
+        private CategoryAxis x_usuarioTransacciones;
+
+        @FXML
+        private Button btn_actualizar;
+
+        @FXML
+        private StackedBarChart<?, ?> stacked_usuarioMasTransacciones;
+
+//        @FXML
+//        void 231e7f(ActionEvent event) {
+//
+//        }
+//        @FXML
+//        void onActualizar(ActionEvent event) {
+//
+//        }
+//
+//        @FXML
+//        void 231e7f(ActionEvent event) {
+//
+//        }
+//
+//        @FXML
+//        void onVolver(ActionEvent event) {
+//
+//        }
+
+
+
 }
---
+++ src/main/java/co/edu/uniquindio/poo/billeteravirtual/viewController/ModificarPerfilAdministradorViewController.java
@@ -0,0 +1,67 @@
+package co.edu.uniquindio.poo.billeteravirtual.viewController;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+public class ModificarPerfilAdministradorViewController {
+
+        @FXML
+        private TextField txf_nombre;
+
+        @FXML
+        private TextField txf_telefono;
+
+        @FXML
+        private ImageView img_logo;
+
+        @FXML
+        private TextField txf_cedula;
+
+        @FXML
+        private Button btn_actualizarAdministrador;
+
+        @FXML
+        private AnchorPane ap_modificarPerfil;
+
+        @FXML
+        private TextField txf_correo;
+
+        @FXML
+        private Button btn_volver;
+
+        @FXML
+        private Text txt_subtitulo;
+
+        @FXML
+        private Text txt_titulo;
+
+        @FXML
+        private TextField txf_contrasenia;
+
+//        @FXML
+//        void 231e7f(ActionEvent event) {
+//
+//        }
+//    @FXML
+//    void 27b0da(ActionEvent event) {
+//
+//    }
+
+        @FXML
+        void onVolver(ActionEvent event) {
+
+        }
+
+        @FXML
+        void onActualizarAdministrador(ActionEvent event) {
+
+        }
+
+
+
+}
